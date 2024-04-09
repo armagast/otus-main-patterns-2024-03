b9d3691e668f6c69c1d16c1f1a6eee5149810fd4
Aleksey Blokhin <alexey.blohin@gmail.com>
2024-04-10 00:29:17 +0300

    Step (14)

--- hw-001/src/main/java/me/blokhin/homework001/SolveQuadraticEquation.java
+++ hw-001/src/main/java/me/blokhin/homework001/SolveQuadraticEquation.java
@@ -11,6 +11,9 @@ public class SolveQuadraticEquation {
 
     public double[] solve(final double a, final double b, final double c, final double epsilon) {
         assertTrue(Math.abs(a) > epsilon, "{a} must not be in [-epsilon, +epsilon]");
+        assertTrue(!Double.isNaN(a) && Double.isFinite(a), "{a} must be finite number");
+        assertTrue(!Double.isNaN(b) && Double.isFinite(b), "{b} must be finite number");
+        assertTrue(!Double.isNaN(c) && Double.isFinite(c), "{c} must be finite number");
 
         final double d = b * b - 4 * a * c;
 
