1fb9f355c59c7b97a3006b4d1e760c2105bd89c8
Aleksey Blokhin <alexey.blohin@gmail.com>
2024-04-28 21:06:16 +0300

    MacroCommand impl

--- hw-004/src/main/java/me/blokhin/homework004/Assert.java
+++ hw-004/src/main/java/me/blokhin/homework004/Assert.java
@@ -1,13 +1,24 @@
 package me.blokhin.homework004;
 
+import java.util.Collection;
 import java.util.Objects;
 
 public abstract class Assert {
-    public static void isTrue(boolean condition, String message) {
+    public static void isTrue(final boolean condition, final String message) {
         if (!condition) throw new IllegalArgumentException(message);
     }
 
-    public static void notNull(Object object, String message) {
+    public static void noNullElements(final Collection<?> collection, final String message) {
+        if (collection != null) {
+            for (Object element : collection) {
+                if (element == null) {
+                    throw new IllegalArgumentException(message);
+                }
+            }
+        }
+    }
+
+    public static void notNull(final Object object, final String message) {
         if (Objects.isNull(object)) throw new IllegalArgumentException(message);
     }
 
